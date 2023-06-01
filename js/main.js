1005fc202b767c328984197044f30471167169a7
luisbelisario <luisbls77@hotmail.com>
2023-06-01 11:09:12 -0300

    incluindo javascript no projeto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const robotron = document.querySelector('#robotron')
+
+robotron.addEventListener('click', () => {
+    console.log('Dominação Mundial!!!')
+})
+
+// Em funções anônimas, não sei o nome dela, portanto ela é chamada só quando o evento for disparado
+// Funções nomeadas são interessantes para quando quero chamá-la outras vezes no código, ela é conhecida, a chamo pelo nome
