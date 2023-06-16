f499231c13ba2e8ae64a69386f440b80195331a6
luisbelisario <luisbls77@hotmail.com>
2023-06-16 11:21:41 -0300

    adicionando controle de estatisticas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 const controles = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controles.forEach((elemento) => {
     elemento.addEventListener('click', (evento) => {
@@ -7,6 +40,8 @@ controles.forEach((elemento) => {
         console.log(evento.target.dataset.controle)
         console.log(evento.target.parentNode)
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        // aqui vou passar como parâmetro o valor do data-peca
+        atualizaEstatisticas(evento.target.dataset.peca)
     })
 })
 
@@ -19,6 +54,16 @@ function manipulaDados(operacao, controle) {
     } else {
         contador.value = parseInt(contador.value) + 1
     }
+}
+
+function atualizaEstatisticas(peca) {
+    console.log(pecas[peca])
+    estatisticas.forEach((estatisticaArray) => {
+        console.log(estatisticaArray.textContent)
+        console.log(estatisticaArray.dataset.estatistica)
+        let nomeEstatistica = estatisticaArray.dataset.estatistica
+        estatisticaArray.textContent = parseInt(estatisticaArray.textContent) + pecas[peca][nomeEstatistica]
+    })
 }
 
 // Em funções anônimas, não sei o nome dela, portanto ela é chamada só quando o evento for disparado
